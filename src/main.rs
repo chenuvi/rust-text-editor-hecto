980e3fa1176d0ea5a1dbdcb13970ce8f1f3261ec
kunchen16 <kunchen16@iflytek.com>
2024-07-23 16:06:44 +0800

    Feat: Separate Files

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,8 @@
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-use std::io::{self, Read};
+mod editor;
+
+use editor::Editor;
 
 fn main() {
-    enable_raw_mode().unwrap();
-    for b in io::stdin().bytes() {
-        let c = b.unwrap() as char;
-        println!("char: {}", c);
-        if c == 'q' {
-            disable_raw_mode().unwrap();
-            break;
-        }
-    }
+    let editor = Editor::default();
+    editor.run();
 }
